b63c61e3be6b121eb6e149bad17df555ff05cfa2
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-03-23 23:48:34 -0700

    created base html skeleton. Wrote pseudocode for JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// there will be a timer that will timeout the gameplay if depleted
+// you will have 10 seconds per question
+// There will be an object that is an array with questions and a correct answer property and incorrect answer properties
+// A question will randomly be selected from the object and the correct guess will be randomly assigned to button 1-4. incorrect guesses will be randomly assigned to the other buttons
+// This question will be removed from the object
+// user will click button they think contains the correct answer
+// on click:
+    // if correct, display screen that says correct! repeats the answer and displays a picture. guessesCorrect++
+    // If incorrect, display screen that says incorrect! displays correct answer and displays a picture. guessesIncorrect--
+    // After 5 seconds on either screen, reload the first page with another random question from the questions array
+// after 5 questions are done, display final results screen
+    // show total guessesCorrect, guessesIncorrect
+    // Write: thanks for playing!
+
