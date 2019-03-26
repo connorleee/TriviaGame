c9dfda39034fa4342c230c39ebd62bb57de93005
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-03-25 20:50:52 -0700

    populated buttons with random responses to a particular question.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
                 { text: "Mt. Mckinley - Denali National Park, Alaska", correct: true },
                 { text: "Mt. Rainier - Mount Rainier National Park, Washinton", correct: false },
                 { text: "Longs Peak - Rocky Mountain National Park, Colorado", correct: false },
-                { text: "Half Dome - Yosemite National Park, California", correct: false },
+                { text: "Half Dome - Yosemite National Park, California", correct: false }
             ],
             correctExtension: " at 20,302 ft!",
             image: "../images/Mckinley.jpg"
@@ -40,7 +40,7 @@ $(document).ready(function () {
                 { text: "Mammoth Cave National Park, Kentucky", correct: true },
                 { text: "Carlsbad Caverns, New Mexico", correct: false },
                 { text: "Wind Cave National Park, South Dakota", correct: false },
-                { text: "Jewel Cave National Monument", correct: false },
+                { text: "Jewel Cave National Monument", correct: false }
             ],
             correctExtension: " at 3454 mapped miles!",
             image: "../images/Mammoth.jpg"
@@ -48,11 +48,11 @@ $(document).ready(function () {
 
         q3 = {
             question: "What state is the only state in the country to not have a National Park or Monument?",
-            respsonses: [
+            responses: [
                 { text: "Delaware", correct: true },
                 { text: "Rhode Island", correct: false },
                 { text: "Kansas", correct: false },
-                { text: "Luisiana", correct: false },
+                { text: "Luisiana", correct: false }
             ],
             correctExtension: "",
             image: "../images/Delaware.jpg"
@@ -64,7 +64,7 @@ $(document).ready(function () {
                 { text: "Yellowstone", correct: true },
                 { text: "Great Smokey Mountains", correct: false },
                 { text: "Grand Canyon", correct: false },
-                { text: "Death Valley", correct: false },
+                { text: "Death Valley", correct: false }
             ],
             correctExtension: " which reaches into Wyoming, Montana, and Idaho!",
             image: "../images/Yellowstone.jpg"
@@ -85,24 +85,44 @@ $(document).ready(function () {
 
     var correctAnswers = 0;
     var incorrectAnswers = 0;
+    var unanswered = 0;
     var gameFinished = false;
     var remainingTime = 10;
-    // var timeLimit = remainingTime;
     var resultTime = 3;
 
     // ensures timer doesn't start ticking down faster
     var timeRunning = false;
-    // variable to store the timer
-    var intervalID;
+    // variables to store the interval and timer
+    var intervalId;
+    var timerId;
+
+    function questionTimer(){
+        // question timer function that runs for 20 seconds
+        timerId = setTimeout(timeOutResult, 1000 * 10);
+    }
+
 
     function resetGameplay() {
         $("#start-screen").show();
+        $("#answer-results").hide();
+        $("#question-screen").hide();
+        $("#final-results").hide();
+
         remainingTime = 10;
+        // shuffle the question objects array to determine which question to ask first
+        shuffleArray(gameInfo);
+        console.log(shuffleArray(gameInfo));
     };
 
     // Start game when game start screen button is clicked. hide start screen, show question screen
     $("#start").click(function () {
-        chooseQuestion();
+        // create a function that is called on game start. this runs a for loop through the gameInfo array
+        // each loop will run the core game logic
+        for (let i = 0; i < gameInfo.length; i++) {
+            loadQuestion(gameInfo[i]);
+        }
+
+        finalResults();
     })
 
     // TODO: create correct answer result function
@@ -116,11 +136,11 @@ $(document).ready(function () {
 
         correctAnswers++
 
-        setTimeout(chooseQuestion, 1000 * resultTime);
+        setTimeout(loadQuestion, 1000 * resultTime);
     }
 
     // TODO: create incorrect answer result function
-    function correctAnswerResult() {
+    function incorrectAnswerResult() {
         // display the results page as an incorrect response
         $("#question-screen").hide();
         $("#answer-results").show();
@@ -130,7 +150,7 @@ $(document).ready(function () {
 
         incorrectAnswer++
 
-        setTimeout(chooseQuestion, 1000 * resultTime);
+        setTimeout(loadQuestion, 1000 * resultTime);
     }
 
     // shows the result of the user's guess or if they timed out. 
@@ -142,9 +162,9 @@ $(document).ready(function () {
         // TODO: $("#correct-answer").text(this.correct)
         // TODO: $("#correct-image").attr("src","this.image")
 
-        incorrectAnswers++
+        unanswered++
 
-        setTimeout(chooseQuestion, 1000 * resultTime);
+        setTimeout(loadQuestion, 1000 * resultTime);
     }
 
     function countDown() {
@@ -152,27 +172,39 @@ $(document).ready(function () {
         $("#time-remaining").text(remainingTime);
     }
 
-    function chooseQuestion() {
+    function shuffleArray(a) {
+        console.log("shuffle: " + a);
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+    function loadQuestion(q) {
+        console.log("question: "+ q);
+        console.log({q});
         $("#answer-results").hide();
         $("#start-screen").hide();
         $("#question-screen").show();
         remainingTime = 10;
         $("#time-remaining").text(remainingTime);
 
+        // shuffle the order of responses
+        console.log("about to shuffle responses: " + q.responses);
+        var shuffledResponses = shuffleArray(q.responses);
+        console.log("shuffled responses: " + shuffledResponses);
 
-        // Choose random q# from the gameInfo array
-        var currentQuestion = gameInfo[Math.floor(Math.random() * gameInfo.length)];
-        console.log("Current question: " + currentQuestion.question);
-        console.log("Correct answer: " + currentQuestion.correct);
+        $("#question").text(q.question);
+        $("#answer1").text(q.responses[0].text);
+        $("#answer2").text(q.responses[1].text);
+        $("#answer3").text(q.responses[2].text);
+        $("#answer4").text(q.responses[3].text);
 
-        $("#question").text(currentQuestion.question);
-        $("#answer1").text("testing a1");
-        $("#answer2").text("testing a2");
-        $("#answer3").text("testing a3");
-        $("#answer4").text("testing a4");
-
-        // question timer function that runs for 20 seconds, then displays the results page for an incorrect response
-        setTimeout(timeOutResult, 1000 * 10);
+        questionTimer();
 
         // Counts down
         if (!timeRunning) {
@@ -180,11 +212,24 @@ $(document).ready(function () {
             timeRunning = true;
         }
 
-        // TODO: remove currentQuestion from the gameInfo array
         // TODO: randomly assign answers to the buttons
         // TODO: provide logic if a button is clicked... correct or incorrect
     }
 
+    function finalResults() {
+        $("#answer-results").hide();
+        $("#start-screen").hide();
+        $("#question-screen").hide();
+        $("#final-results").show();
+
+        $("#number-correct").text(correctAnswers);
+        $("#number-incorrect").text(incorrectAnswers);
+        $("#number-unanswered").text(unanswered);
+
+        $("#play-again").click(function () {
+            resetGameplay();
+        });
+    }
 
     resetGameplay();
 })
